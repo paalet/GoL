c1109284cbfcfa6da014ea083df5c5a34ca56ecb
SimenPA <simenp.andersen@gmail.com>
2017-02-14 13:55:10 +0100

    Canvas kan tegnes, og størrelse kan endres. Nå kan også farge settes på celler.

--- src/controller/mainScreenController.java
+++ src/controller/mainScreenController.java
@@ -6,7 +6,9 @@ import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.event.ActionEvent;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Slider;
+import javafx.scene.paint.Color;
 import model.StaticBoard;
 import model.GoL;
 
@@ -17,12 +19,16 @@ public class mainScreenController implements Initializable {
 
     @FXML private Canvas boardCanvas;
     @FXML private Slider cellSizeSlider;
+    @FXML private ColorPicker cellColorPicker;
     StaticBoard staticBoard = new StaticBoard();
 
     @Override
     public void initialize(java.net.URL location, java.util.ResourceBundle resources){
 
-        cellSizeSlider.setValue(10.0);
+        cellSizeSlider.setValue(50.0);
+        cellColorPicker.setValue(Color.BLACK);
+        GoL.setCellColor(Color.BLACK);
+        GoL.setCellSize(50.0);
         draw();
 
     }
@@ -31,7 +37,7 @@ public class mainScreenController implements Initializable {
     private void draw(){
 
         GraphicsContext gc = boardCanvas.getGraphicsContext2D();
-        staticBoard.draw(gc, GoL.getCellSize());
+        staticBoard.draw(gc, GoL.getCellSize(), GoL.getCellColor());
 
 
 
@@ -44,4 +50,12 @@ public class mainScreenController implements Initializable {
         draw();
 
     }
+
+    public void setCellColorEvent() {
+
+        GoL.setCellColor(cellColorPicker.getValue());
+        draw();
+
+    }
+
 }

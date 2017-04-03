9f204b6790a08dd94e2e77161a7327fb500af6b7
Pål <andreassen.pal@gmail.com>
2017-04-03 10:25:59 +0200

    utvidet test av nextGen til 10 brett + pluss ny intelliJ-versjon har autoformattert whitespaces

--- src/tests/BoardTest.java
+++ src/tests/BoardTest.java
@@ -8,30 +8,29 @@ import org.junit.Test;
 public class BoardTest {
 
     @Test
-    public void testNextGeneration_board1(){
+    public void testNextGeneration_board01_8x8(){
 
         // Arrange
         byte[][] testBoard = {
-                {0,0,1,0,0,0,0,0},
-                {0,0,1,0,0,0,0,0},
-                {0,0,1,0,0,0,0,0},
-                {0,0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,1,1},
-                {0,0,0,0,0,0,1,1}
+                {1,1,1,1,1,1,1,1},
+                {1,0,0,0,0,0,0,1},
+                {1,0,0,0,0,0,0,1},
+                {1,0,0,1,1,0,0,1},
+                {1,0,0,1,1,0,0,1},
+                {1,0,0,0,0,0,0,1},
+                {1,0,0,0,0,0,0,1},
+                {1,1,1,1,1,1,1,1}
         };
         String expectedBoardString = new String(
-                "00000000" +
-                "01110000" +
-                "00000000" +
-                "00000000" +
-                "00000000" +
-                "00000000" +
-                "00000011" +
-                "00000011");
-
-        // Act
+                "11111111" +
+                "10111101" +
+                "11000011" +
+                "11011011" +
+                "11011011" +
+                "11000011" +
+                "10111101" +
+                "11111111"
+        );
         StaticBoard testStaticBoard = new StaticBoard();
         testStaticBoard.setHEIGHT(8);
         testStaticBoard.setWIDTH(8);
@@ -41,6 +40,8 @@ public class BoardTest {
         GoL.setBornAmount(initBornAmount);
         GoL.setSurviveAmount(initSurviveAmount);
         testStaticBoard.setBoard(testBoard);
+
+        // Act
         testStaticBoard.nextGeneration();
 
         // Assert
@@ -50,7 +51,7 @@ public class BoardTest {
 
 
     @Test
-    public void testNextGeneration_board2(){
+    public void testNextGeneration_board02_8x8(){
 
         // Arrange
         byte[][] testBoard = {
@@ -71,9 +72,8 @@ public class BoardTest {
                 "11100011" +
                 "11100011" +
                 "00000011" +
-                "00000000");
-
-        // Act
+                "00000000"
+        );
         StaticBoard testStaticBoard = new StaticBoard();
         testStaticBoard.setHEIGHT(8);
         testStaticBoard.setWIDTH(8);
@@ -83,6 +83,360 @@ public class BoardTest {
         GoL.setBornAmount(initBornAmount);
         GoL.setSurviveAmount(initSurviveAmount);
         testStaticBoard.setBoard(testBoard);
+
+        // Act
+        testStaticBoard.nextGeneration();
+
+        // Assert
+        String actualBoardString = testStaticBoard.toString();
+        Assert.assertEquals(expectedBoardString, actualBoardString);
+    }
+
+
+    @Test
+    public void testNextGeneration_board03_8x8(){
+
+        // Arrange
+        byte[][] testBoard = {
+                {1,1,1,0,0,0,0,1},
+                {0,0,0,0,0,0,0,1},
+                {0,0,0,0,0,0,0,1},
+                {0,0,0,1,0,0,0,0},
+                {0,0,0,1,1,0,0,0},
+                {1,0,0,0,0,0,0,0},
+                {1,0,0,0,0,0,0,0},
+                {1,0,0,0,0,1,1,1}
+        };
+        String expectedBoardString = new String(
+                "01000000" +
+                "01000011" +
+                "00000000" +
+                "00011000" +
+                "00011000" +
+                "00000000" +
+                "11000010" +
+                "00000010"
+        );
+        StaticBoard testStaticBoard = new StaticBoard();
+        testStaticBoard.setHEIGHT(8);
+        testStaticBoard.setWIDTH(8);
+        testStaticBoard.newBoard();
+        int[] initBornAmount = {3};
+        int[] initSurviveAmount = {2,3};
+        GoL.setBornAmount(initBornAmount);
+        GoL.setSurviveAmount(initSurviveAmount);
+        testStaticBoard.setBoard(testBoard);
+
+        // Act
+        testStaticBoard.nextGeneration();
+
+        // Assert
+        String actualBoardString = testStaticBoard.toString();
+        Assert.assertEquals(expectedBoardString, actualBoardString);
+    }
+
+
+    @Test
+    public void testNextGeneration_board04_8x8(){
+
+        // Arrange
+        byte[][] testBoard = {
+                {1,1,1,1,1,1,1,1},
+                {0,0,0,0,0,0,0,1},
+                {1,1,1,1,1,1,1,1},
+                {1,0,0,0,0,0,0,0},
+                {1,1,1,1,1,1,1,1},
+                {0,0,0,0,0,0,0,1},
+                {1,1,1,1,1,1,1,1},
+                {1,0,0,0,0,0,0,0}
+        };
+        String expectedBoardString = new String(
+                "01111111" +
+                "00000000" +
+                "11111111" +
+                "00000000" +
+                "11111111" +
+                "00000000" +
+                "11111111" +
+                "10111110"
+        );
+        StaticBoard testStaticBoard = new StaticBoard();
+        testStaticBoard.setHEIGHT(8);
+        testStaticBoard.setWIDTH(8);
+        testStaticBoard.newBoard();
+        int[] initBornAmount = {3};
+        int[] initSurviveAmount = {2,3};
+        GoL.setBornAmount(initBornAmount);
+        GoL.setSurviveAmount(initSurviveAmount);
+        testStaticBoard.setBoard(testBoard);
+
+        // Act
+        testStaticBoard.nextGeneration();
+
+        // Assert
+        String actualBoardString = testStaticBoard.toString();
+        Assert.assertEquals(expectedBoardString, actualBoardString);
+    }
+
+
+    @Test
+    public void testNextGeneration_board05_8x8(){
+
+        // Arrange
+        byte[][] testBoard = {
+                {1,1,1,0,1,1,1,0},
+                {1,0,1,0,1,0,1,0},
+                {1,0,1,0,1,0,1,0},
+                {1,0,1,0,1,0,1,0},
+                {1,0,1,0,1,0,1,0},
+                {1,0,1,0,1,0,1,0},
+                {1,0,1,0,1,0,1,0},
+                {1,0,1,1,1,0,1,1}
+        };
+        String expectedBoardString = new String(
+                "10101010" +
+                "10101011" +
+                "10101011" +
+                "10101011" +
+                "10101011" +
+                "10101011" +
+                "10101010" +
+                "00101011"
+        );
+        StaticBoard testStaticBoard = new StaticBoard();
+        testStaticBoard.setHEIGHT(8);
+        testStaticBoard.setWIDTH(8);
+        testStaticBoard.newBoard();
+        int[] initBornAmount = {3};
+        int[] initSurviveAmount = {2,3};
+        GoL.setBornAmount(initBornAmount);
+        GoL.setSurviveAmount(initSurviveAmount);
+        testStaticBoard.setBoard(testBoard);
+
+        // Act
+        testStaticBoard.nextGeneration();
+
+        // Assert
+        String actualBoardString = testStaticBoard.toString();
+        Assert.assertEquals(expectedBoardString, actualBoardString);
+    }
+
+
+    @Test
+    public void testNextGeneration_board06_8x8(){
+
+        // Arrange
+        byte[][] testBoard = {
+                {1,1,0,0,0,0,1,1},
+                {1,1,0,0,0,0,1,1},
+                {0,0,1,0,0,1,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,1,0,0,1,0,0},
+                {1,1,0,0,0,0,1,1},
+                {1,1,0,0,0,0,1,1}
+        };
+        String expectedBoardString = new String(
+                "11000011" +
+                "10100101" +
+                "01000010" +
+                "00000000" +
+                "00000000" +
+                "01000010" +
+                "10100101" +
+                "11000011"
+        );
+        StaticBoard testStaticBoard = new StaticBoard();
+        testStaticBoard.setHEIGHT(8);
+        testStaticBoard.setWIDTH(8);
+        testStaticBoard.newBoard();
+        int[] initBornAmount = {3};
+        int[] initSurviveAmount = {2,3};
+        GoL.setBornAmount(initBornAmount);
+        GoL.setSurviveAmount(initSurviveAmount);
+        testStaticBoard.setBoard(testBoard);
+
+        // Act
+        testStaticBoard.nextGeneration();
+
+        // Assert
+        String actualBoardString = testStaticBoard.toString();
+        Assert.assertEquals(expectedBoardString, actualBoardString);
+    }
+
+
+    @Test
+    public void testNextGeneration_board07_6x10(){
+
+        // Arrange
+        byte[][] testBoard = {
+                {1,1,1,1,1,1,1,1,1,1},
+                {1,0,0,0,0,0,0,0,0,1},
+                {1,0,0,0,0,0,0,0,0,1},
+                {1,0,0,0,0,0,0,0,0,1},
+                {1,0,0,0,0,0,0,0,0,1},
+                {1,1,1,1,1,1,1,1,1,1},
+        };
+        String expectedBoardString = new String(
+                "1111111111" +
+                "1011111101" +
+                "1100000011" +
+                "1100000011" +
+                "1011111101" +
+                "1111111111"
+        );
+        StaticBoard testStaticBoard = new StaticBoard();
+        testStaticBoard.setHEIGHT(6);
+        testStaticBoard.setWIDTH(10);
+        testStaticBoard.newBoard();
+        int[] initBornAmount = {3};
+        int[] initSurviveAmount = {2,3};
+        GoL.setBornAmount(initBornAmount);
+        GoL.setSurviveAmount(initSurviveAmount);
+        testStaticBoard.setBoard(testBoard);
+
+        // Act
+        testStaticBoard.nextGeneration();
+
+        // Assert
+        String actualBoardString = testStaticBoard.toString();
+        Assert.assertEquals(expectedBoardString, actualBoardString);
+    }
+
+
+    @Test
+    public void testNextGeneration_board08_5x10(){
+
+        // Arrange
+        byte[][] testBoard = {
+                {0,0,1,0,0},
+                {0,0,1,0,0},
+                {1,1,1,1,1},
+                {0,0,1,0,0},
+                {0,0,1,0,0},
+                {0,0,1,0,0},
+                {0,0,1,0,0},
+                {1,1,1,1,1},
+                {0,0,1,0,0},
+                {0,0,1,0,0}
+        };
+        String expectedBoardString = new String(
+                "00000" +
+                "00000" +
+                "00000" +
+                "00000" +
+                "01110" +
+                "01110" +
+                "00000" +
+                "00000" +
+                "00000" +
+                "00000"
+        );
+        StaticBoard testStaticBoard = new StaticBoard();
+        testStaticBoard.setHEIGHT(10);
+        testStaticBoard.setWIDTH(5);
+        testStaticBoard.newBoard();
+        int[] initBornAmount = {3};
+        int[] initSurviveAmount = {2,3};
+        GoL.setBornAmount(initBornAmount);
+        GoL.setSurviveAmount(initSurviveAmount);
+        testStaticBoard.setBoard(testBoard);
+
+        // Act
+        testStaticBoard.nextGeneration();
+
+        // Assert
+        String actualBoardString = testStaticBoard.toString();
+        Assert.assertEquals(expectedBoardString, actualBoardString);
+    }
+
+
+    @Test
+    public void testNextGeneration_board09_10x10(){
+
+        // Arrange
+        byte[][] testBoard = {
+                {0,0,0,0,0,0,0,0,0,0},
+                {0,1,1,1,1,1,1,1,1,0},
+                {0,1,1,1,1,1,1,1,1,0},
+                {0,1,1,0,0,0,0,1,1,0},
+                {0,1,1,0,0,0,0,1,1,0},
+                {0,1,1,0,0,0,0,1,1,0},
+                {0,1,1,0,0,0,0,1,1,0},
+                {0,1,1,1,1,1,1,1,1,0},
+                {0,1,1,1,1,1,1,1,1,0},
+                {0,0,0,0,0,0,0,0,0,0}
+        };
+        String expectedBoardString = new String(
+                "0011111100" +
+                "0100000010" +
+                "1000000001" +
+                "1000110001" +
+                "1001001001" +
+                "1001001001" +
+                "1000110001" +
+                "1000000001" +
+                "0100000010" +
+                "0011111100"
+        );
+        StaticBoard testStaticBoard = new StaticBoard();
+        testStaticBoard.setHEIGHT(10);
+        testStaticBoard.setWIDTH(10);
+        testStaticBoard.newBoard();
+        int[] initBornAmount = {3};
+        int[] initSurviveAmount = {2,3};
+        GoL.setBornAmount(initBornAmount);
+        GoL.setSurviveAmount(initSurviveAmount);
+        testStaticBoard.setBoard(testBoard);
+
+        // Act
+        testStaticBoard.nextGeneration();
+
+        // Assert
+        String actualBoardString = testStaticBoard.toString();
+        Assert.assertEquals(expectedBoardString, actualBoardString);
+    }
+
+
+    @Test
+    public void testNextGeneration_board10_10x10(){
+
+        // Arrange
+        byte[][] testBoard = {
+                {0,1,0,0,1,0,0,1,0,0},
+                {0,1,0,0,1,0,0,1,0,0},
+                {0,1,0,0,1,0,0,1,0,0},
+                {0,0,0,0,0,0,0,0,0,0},
+                {0,1,0,0,1,0,0,1,0,0},
+                {0,1,0,0,1,0,0,1,0,0},
+                {0,1,0,0,1,0,0,1,0,0},
+                {0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0},
+                {0,0,1,1,1,0,0,1,1,1}
+        };
+        String expectedBoardString = new String(
+                "0000000000" +
+                "1111111110" +
+                "0000000000" +
+                "0000000000" +
+                "0000000000" +
+                "1111111110" +
+                "0000000000" +
+                "0000000000" +
+                "0001000010" +
+                "0001000010"
+        );
+        StaticBoard testStaticBoard = new StaticBoard();
+        testStaticBoard.setHEIGHT(10);
+        testStaticBoard.setWIDTH(10);
+        testStaticBoard.newBoard();
+        int[] initBornAmount = {3};
+        int[] initSurviveAmount = {2,3};
+        GoL.setBornAmount(initBornAmount);
+        GoL.setSurviveAmount(initSurviveAmount);
+        testStaticBoard.setBoard(testBoard);
+
+        // Act
         testStaticBoard.nextGeneration();
 
         // Assert
@@ -118,8 +472,6 @@ public class BoardTest {
                 "0000000101" +
                 "0000000011" +
                 "0000000010");
-
-        // Act
         StaticBoard testStaticBoard = new StaticBoard();
         testStaticBoard.setHEIGHT(10);
         testStaticBoard.setWIDTH(10);
@@ -128,6 +480,8 @@ public class BoardTest {
         int[] initSurviveAmount = {2,3};
         GoL.setBornAmount(initBornAmount);
         GoL.setSurviveAmount(initSurviveAmount);
+
+        // Act
         testStaticBoard.setBoard(testBoard);
         for (int i = 0; i < 28; i++) {
             testStaticBoard.nextGeneration();

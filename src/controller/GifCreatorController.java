6645bf72f85c945ebb04f5a35e932a8cba0c0620
Pål <andreassen.pal@gmail.com>
2017-04-19 16:20:47 +0200

    work in progress oppretter GifCreator

---
+++ src/controller/GifCreatorController.java
@@ -0,0 +1,28 @@
+package controller;
+
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GifCreatorController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void createGifEvent() {
+
+        
+    }
+
+
+    public void cancelGifEvent() {
+
+
+    }
+
+}

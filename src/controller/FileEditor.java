0fde3bc9a1476407234069120e0ea00d4547174e
SimenPA <simenp.andersen@gmail.com>
2017-04-20 15:15:51 +0200

    Fikset litt mer på saveFile, lagt til startmeny som ikke funker

--- src/controller/FileEditor.java
+++ src/controller/FileEditor.java
@@ -2,12 +2,36 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
+
+import java.awt.*;
+import java.awt.TextArea;
+import java.awt.TextField;
 
 /**
  * Created by simenperschandersen on 19.04.2017.
  */
 public class FileEditor {
+    @FXML
+    private TextField saveWindowTitleField;
+    @FXML
+    private TextField saveWindowOriginField;
+    @FXML
+    private TextField saveWindowCommentsField;
+
+    public void setTitle(String title) {
+        saveWindowTitleField.setText(title);
+
+    }
+
+    public void setOrigin(String origin) {
+        saveWindowOriginField.setText(origin);
+
+    }
+    public void setComments(String comments) {
+        saveWindowCommentsField.setText(comments);
+
+    }
 
 
 }

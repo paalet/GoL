8eef48c66f58ef73ea96011dc5329b5009866a35
Pål <andreassen.pal@gmail.com>
2017-04-25 20:16:12 +0200

    Implementert valgfrie variabler i GifCreator

--- src/model/GifCreator.java
+++ src/model/GifCreator.java
@@ -1,19 +1,24 @@
 package model;
 
 
+import javafx.stage.FileChooser;
+
+import javax.swing.filechooser.FileSystemView;
+
 public class GifCreator {
 
     private static String path;
-    private static int timePerMilliSecond;
+    private static int milliSecondsPerGen;
     private static int cellSize;
     private static int imageWidth;
     private static int imageHeight;
     private static java.awt.Color aliveCellColor;
     private static java.awt.Color deadCellColor;
 
-    public static void writeGif(lieng.GIFWriter gwriter, Board gifBoard, int genCounter) throws Exception {
 
-        if (genCounter == 0) {
+    public static void writeGif(lieng.GIFWriter gwriter, Board gifBoard, int genCount) throws Exception {
+
+        if (genCount == 0) {
 
             drawGifImage(gwriter, gifBoard);
             gwriter.insertCurrentImage();
@@ -24,7 +29,7 @@ public class GifCreator {
             drawGifImage(gwriter, gifBoard);
             gwriter.insertAndProceed();
             gifBoard.nextGeneration();
-            writeGif(gwriter, gifBoard, genCounter - 1);
+            writeGif(gwriter, gifBoard, genCount - 1);
         }
     }
 
@@ -38,11 +43,11 @@ public class GifCreator {
 
                     if (((StaticBoard) gifBoard).getCurrentBoard()[y][x] == 1) {
 
-                        gwriter.fillRect(x * cellSize, ((x + 1) * cellSize) - 3, y * cellSize, ((y + 1) * cellSize) - 3, aliveCellColor);
+                        gwriter.fillRect((x * cellSize) + 1, ((x + 1) * cellSize) - 1, (y * cellSize) + 1, ((y + 1) * cellSize) - 1, aliveCellColor);
 
                     } else {
 
-                        gwriter.fillRect(x * cellSize, ((x + 1) * cellSize) - 3, y * cellSize, ((y + 1) * cellSize) - 3, deadCellColor);
+                        gwriter.fillRect((x * cellSize) + 1, ((x + 1) * cellSize) - 1, (y * cellSize) + 1, ((y + 1) * cellSize) - 1, deadCellColor);
                     }
                 }
             }
@@ -67,31 +72,70 @@ public class GifCreator {
     }
 
 
-    public static void calculateImageSize(Board gifBoard) {
+    public static void setImageSize(Board gifBoard, String dim, int inputSize) {
 
         if (gifBoard instanceof StaticBoard) {
 
-            imageHeight = ((StaticBoard) gifBoard).getCurrentBoard().length * cellSize;
-            imageWidth = ((StaticBoard) gifBoard).getCurrentBoard()[0].length * cellSize;
+            // Calculate image and cell sizes from height input
+            if (dim == "height") {
 
+                cellSize = inputSize / ((StaticBoard) gifBoard).getCurrentBoard().length;
+                imageHeight = cellSize * ((StaticBoard) gifBoard).getCurrentBoard().length;
+                imageWidth = cellSize * ((StaticBoard) gifBoard).getCurrentBoard()[0].length;
+
+                // Calculate image and cell sizes from width input
+            } else if (dim == "width") {
+
+                cellSize = inputSize / ((StaticBoard) gifBoard).getCurrentBoard()[0].length;
+                imageWidth = cellSize * ((StaticBoard) gifBoard).getCurrentBoard()[0].length;
+                imageHeight = cellSize * ((StaticBoard) gifBoard).getCurrentBoard().length;
+            }
         } else if (gifBoard instanceof DynamicBoard) {
 
-            imageHeight = ((DynamicBoard) gifBoard).getCurrentBoard().size() * cellSize;
-            imageWidth = ((DynamicBoard) gifBoard).getCurrentBoard().get(0).size() * cellSize;
+            // Calculate image and cell sizes from height input
+            if (dim == "height") {
+
+                cellSize = inputSize / ((DynamicBoard) gifBoard).getCurrentBoard().size();
+                imageHeight = cellSize * ((DynamicBoard) gifBoard).getCurrentBoard().size();
+                imageWidth = cellSize * ((DynamicBoard) gifBoard).getCurrentBoard().get(0).size();
+
+                // Calculate image and cell sizes from width input
+            } else if (dim == "width") {
+
+                cellSize = inputSize / ((DynamicBoard) gifBoard).getCurrentBoard().get(0).size();
+                imageWidth = cellSize * ((DynamicBoard) gifBoard).getCurrentBoard().get(0).size();
+                imageHeight = cellSize * ((DynamicBoard) gifBoard).getCurrentBoard().size();
+            }
         }
     }
 
+    public static void calculateAndSetMilliSecondsPerGen(int gps) {
+
+        milliSecondsPerGen = 1000 / gps;
+    }
+
+    public static boolean inputPathfromFileChooser(boolean ok) {
+
+        FileChooser pathChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("GIF files (*.gif)", "*.gif");
+                pathChooser.getExtensionFilters().add(extFilter);
+                pathChooser.setInitialDirectory(FileSystemView.getFileSystemView().getHomeDirectory());
+        try {
+            GifCreator.setPath(pathChooser.showSaveDialog(null).getPath());
+        } catch(NullPointerException e) {
+            new CustomDialog("GIF not created", true,
+                    "GIF creation canceled. Choose a filename and target directory to create GIF.");
+            ok = false;
+        }
+        return ok;
+    }
 
     public static String getPath() {
         return path;
     }
 
-    public static int getTimePerMilliSecond() {
-        return timePerMilliSecond;
-    }
-
-    public static int getCellSize() {
-        return cellSize;
+    public static int getMilliSecondsPerGen() {
+        return milliSecondsPerGen;
     }
 
     public static int getImageWidth() {
@@ -106,12 +150,16 @@ public class GifCreator {
         GifCreator.path = path;
     }
 
-    public static void setTimePerMilliSecond(int timePerMilliSecond) {
-        GifCreator.timePerMilliSecond = timePerMilliSecond;
-    }
-
     public static void setCellSize(int cellSize) {
         GifCreator.cellSize = cellSize;
+    }
+
+    public static void setImageWidth(int imageWidth) {
+        GifCreator.imageWidth = imageWidth;
+    }
+
+    public static void setImageHeight(int imageHeight) {
+        GifCreator.imageHeight = imageHeight;
     }
 
     public static void setAliveCellColor(java.awt.Color aliveCellColor) {

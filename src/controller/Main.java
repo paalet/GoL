641d6fef53a4b182e4ba28c6eed7f6549c61a341
Pål <andreassen.pal@gmail.com>
2017-05-04 13:37:12 +0200

    Main ducumented

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,16 +1,21 @@
 package controller;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-
+/**
+ * Application class that launches the application and sets the primary Stage.
+ */
 public class Main extends Application {
 
+    /**
+     * Loads menu.fxml and sets it as the Scene of primary Stage.
+     * @param primaryStage is the default starting Stage of the application
+     * @throws Exception if menu.fxml can not be loaded.
+     */
     @Override
     public void start(Stage primaryStage) throws Exception{
 
@@ -22,11 +27,18 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    /**
+     * Ensures exit of the application.
+     */
     @Override
     public void stop() {
         System.exit(0);
     }
 
+    /**
+     * The entry point of the application.
+     * @param args the command line arguments passed to the application
+     */
     public static void main(String[] args) {
 
       launch(args);
